4a3cee41a6ad483885a0ba7270b1a6696577c232
Collin McCartney <collinpmccartney@gmail.com>
2024-06-09 15:57:48 -0400

    added JS functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,49 @@
+var wordBlanks = document.querySelector(".word-blanks");
 var startButton = document.querySelector(".start-button");
 var timer = document.querySelector(".timer-count");
+var winText = document.querySelector(".win");
+var loseText = document.querySelector(".lose");
 var secondsLeft = 10;
 var wins = 0;
 var losses = 0;
-var guessWords = ["JavaScript", "CSS", "HTML", "Python", "React", "Java", "Node", "Express"];
+var guessWords = ["javaScript", "cSS", "hTML", "python", "react", "java", "node", "express"];
+var underscoreArr = [];
+var isWin = false;
+
+function init() {
+    getWins();
+}
 
 
 
 function startGame() {
-startTimer();
+   startTimer();
+   startButton.disabled = true;
 
+
+}
+
+function setWins() {
+    wins++;
+winText.textContent = wins;
+localStorage.setItem("wins-saved", wins);
+}
+
+function getWins() {
+    var winsSaved = localStorage.getItem("wins-saved");
+    winText.textContent = winsSaved;
 }
 
 function startTimer() {
-    var timerInterval = setInterval(function() {
-        secondsLeft--;
-        timer.textContent = secondsLeft;
+var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timer.textContent = secondsLeft;
 
-        if (secondsLeft === 0) {
-            clearInterval(timerInterval);
-            secondsLeft = 10;
-            timer.textContent = secondsLeft;
-        }
-    }, 1000);
+if (secondsLeft === 0) {
+    clearInterval(timerInterval);
+    secondsLeft = 10;
+    timer.textContent = secondsLeft;
 }
+}, 1000)};
 
 startButton.addEventListener("click", startGame);
